245b96ed08e8a765adb653701a6883ef3d5b3d3e
thanhtunguong <utititung@gmail.com>
2016-03-13 17:19:51 +0700

    Thêm sự kiện khi back từ activity Ghi chú

--- ProjectDA/src/com/doan/lichhoctap/GhiChuActivity.java
+++ ProjectDA/src/com/doan/lichhoctap/GhiChuActivity.java
@@ -419,9 +419,10 @@ public class GhiChuActivity extends ActionBarActivity implements OnClickListener
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			onBackPressed();
+			break;
 		}
 		return super.onOptionsItemSelected(item);
 	}
